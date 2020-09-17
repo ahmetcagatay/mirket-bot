2b5c523483909af41cca75f8a96094cfcc78f0fb
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2020-09-17 16:09:29 +0300

    changed some codes

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import discord
+# Discord kütüphanesinden komut bölümü uygulama içine alındı.
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        await bot.change_presence(activity=discord.Game(name="!yardim"))
+# Komut özelliğinden yararlanılarak Discord botunun önismi ayarlandı.
+bot = commands.Bot(command_prefix='!')
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@bot.event # Bot için event belirlendi.
+async def on_ready(): #Bot hazır olduğunda yapılacak işlemler için fonksiyon belirlendi.
+    print(f'{bot.user.name} artık aktif!') # Botun aktif olduğunda komut istemcisine ismi ile birlikte aktif olduğunu belirten bir yazı gönderilecek.
+    await bot.change_presence(activity=discord.Game(name="!yardım"))
 
 
-
-client = MyClient()
 to = "NzE0MjA1ODc0NzA0NDgyMzc1"
 ke = ".XsrSDQ."
 n = "xYJQ9jwMnniLzktRKX5HiD487vc"
 
 
-client.run(to+ke+n)
+bot.run(to+ke+n)
