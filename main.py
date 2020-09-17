748b6eee092fed4963ecede8181dd23ea09951a7
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2020-09-17 15:28:32 +0300

    hs 1

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import discord
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        await bot.change_presence(activity=discord.Game(name="!yardım"))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+
+
 
 client = MyClient()
 to = "NzE0MjA1ODc0NzA0NDgyMzc1"
