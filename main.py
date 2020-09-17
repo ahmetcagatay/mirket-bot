77fb826c1ef9d9e71b3adc3dcd431721ba5854bc
Ahmet Çağatay <ahmetsalihcagatay@gmail.com>
2020-09-17 16:20:55 +0300

    imports try 2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import discord
 import os
 import random
+
+import asyncio
+import aiohttp
+import json
+
+
 # Discord kütüphanesinden komut bölümü uygulama içine alındı.
 from discord.ext import commands
 
